459f2969aa9d4c9d0795b2aec5572faeb83d3289
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2023-08-04 11:25:57 -0400

    update functions

--- src/palindromes/palindromes.js
+++ src/palindromes/palindromes.js
@@ -1,8 +1,24 @@
-const Stack = require("../lib/stack");
+const Stack = require('../lib/stack');
 
-const isPalindrome = (sentence) => {
-  sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
-  // Your solution here
+const isPalindrome = sentence => {
+  sentence = sentence.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  const middle = Math.floor(sentence.length / 2);
+  const stack = new Stack();
+
+  //push all the letters up to middle into a new stack
+  for (let index = 0; index < middle; index++) {
+    stack.push(sentence[index]);
+  }
+  //check if middle is odd
+  middle += sentence.length % 2 === 0 ? 0 : 1;
+
+  //compare two stacks, if letters are ever not equal return false
+  for (let index = middle, limit = sentence.length; index < limit; index++) {
+    if (sentence[index] !== stack.pop) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = isPalindrome;
--- src/parentheses/parentheses.js
+++ src/parentheses/parentheses.js
@@ -1,5 +1,21 @@
-const Stack = require("../lib/stack");
+const Stack = require('../lib/stack');
 
-const match = (expression) => {};
+const match = expression => {
+  const stack = new Stack();
+  for (let index = 0; index < expression.length; index++) {
+    if (expression[index] === '(') {
+      stack.push('(');
+    } else {
+      if (expression[index] === ')') {
+        if (stack.top) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return !stack.top;
+};
 
 module.exports = match;
